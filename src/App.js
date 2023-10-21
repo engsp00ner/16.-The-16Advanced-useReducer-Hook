b747dfc8c5596e3ba8bbe4bd4dfa6bb32851513f
Ahmed Mostafa <Engsp00ner51@gmail.com>
2023-10-21 15:00:19 +0300

    React quiz

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-import DateCounter from "./DateCounter";
+import "./App.css";
+import Header from "./Header";
+import Main from "./main";
 export default function App() {
   return (
-    <div>
-      <DateCounter/>
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions?</p>
+      </Main>
     </div>
   );
 }
-
