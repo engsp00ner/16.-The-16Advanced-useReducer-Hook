66eb47ca3e9083cd6f2d373a7c4d7aa21fd53f22
Ahmed Mostafa <Engsp00ner51@gmail.com>
2023-10-22 16:58:44 +0300

    6.loading Question from

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Main from "./main";
 export default function App() {
+
+  useEffect(function () {
+    async function GetQuestions(){
+      try{
+        const res = await fetch("http://localhost:8000/questions");
+        console.log(res);
+        if(!res.ok)
+        throw new Error("Something went wrong can`t load data ");
+      const data =await res.json();
+      console.log(data);
+      }
+      catch(err){
+        console.log(err);
+      }
+      finally{}
+
+    }
+    GetQuestions();},[]);
+  
   return (
     <div className="app">
       <Header />
